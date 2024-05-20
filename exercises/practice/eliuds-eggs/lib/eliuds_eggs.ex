621f6eae09a0f617919bc67dfa7fe42dac98b121
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-05-20 14:32:20 -0500

    finish egg

--- exercises/practice/eliuds-eggs/lib/eliuds_eggs.ex
+++ exercises/practice/eliuds-eggs/lib/eliuds_eggs.ex
@@ -1,8 +1,35 @@
 defmodule EliudsEggs do
   @doc """
-  Given the number, count the number of eggs.
+  This is a decimal to binary converter, then getting a sum of 1s
+
+  As seen here: https://www.wikihow.com/Convert-from-Decimal-to-Binary
   """
-  @spec egg_count(number :: integer()) :: non_neg_integer()
+
+  def egg_count(0) do
+    0
+  end
+
   def egg_count(number) do
+    number
+    |> recurse("")
+    |> String.split("", trim: true)
+    |> Enum.map(fn
+      "0" -> 0
+      "1" -> 1
+    end)
+    |> Enum.sum()
+  end
+
+  # base case
+  defp recurse(0, binary) do
+    binary
+  end
+
+  # recursion
+  defp recurse(target, binary) do
+    divided = div(target, 2)
+    remainder = rem(target, 2)
+
+    recurse(divided, "#{remainder}" <> binary)
   end
 end
